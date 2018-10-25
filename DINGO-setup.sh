e44913531ddd3a75b8674dd3c0218588f368ab34
MikeP243 <44385091+MikeP243@users.noreply.github.com>
2018-10-25 19:27:42 +1000

    Easy-Peasy

---
+++ DINGO-setup.sh
@@ -0,0 +1,160 @@
+#/bin/bash
+
+cd ~
+echo "****************************************************************************"
+echo "* Ubuntu 14.04 is the recommended opearting system for this install.       *"
+echo "*                                                                          *"
+echo "* This script will install and configure your DingoDollars masternodes.  *"
+echo "****************************************************************************"
+echo && echo && echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo && echo && echo
+
+echo "Do you want to install all needed dependencies (no if you did it before)? [y/n]"
+read DOSETUP
+
+if [[ $DOSETUP =~ "y" ]] ; then
+  sudo apt-get update
+
+  sudo apt-get upgrade
+
+  sudo apt-get install automake libdb++-dev build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libminiupnpc-dev git software-properties-common python-software-properties g++ bsdmainutils libevent-dev -y
+
+  sudo add-apt-repository -y ppa:bitcoin/bitcoin
+
+  sudo apt-get update
+
+  sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+
+  sudo apt-get install libgmp3-dev -y
+
+
+  git clone https://github.com/MikeP243/DingoDollars
+
+cd DingoDollars/
+
+sudo dd if=/dev/zero of=/swapfile bs=1M count=2000
+sudo mkswap /swapfile
+sudo chown root:root /swapfile
+sudo chmod 0600 /swapfile
+sudo swapon /swapfile
+
+#make swap permanent
+sudo echo "/swapfile none swap sw 0 0" >> /etc/fstab
+
+cd src
+
+make -f makefile.unix
+
+mv DingoDollarsd /usr/local/bin/
+
+cd
+
+
+  # dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
+  # mkswap /var/swap.img
+  # swapon /var/swap.img
+
+  # sudo mv  DingoDollars/bin/* /usr/bitcoin
+
+  cd
+
+  sudo apt-get install -y ufw
+  sudo ufw allow ssh/tcp
+  sudo ufw limit ssh/tcp
+  sudo ufw logging on
+  echo "y" | sudo ufw enable
+  sudo ufw status
+
+  mkdir -p ~/bin
+  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+  source ~/.bashrc
+fi
+
+## Setup conf
+mkdir -p ~/bin
+echo ""
+echo "Configure your masternodes now!"
+echo "Type the IP of this server, followed by [ENTER]:"
+read IP
+
+MNCOUNT=""
+re='^[0-9]+$'
+while ! [[ $MNCOUNT =~ $re ]] ; do
+   echo ""
+   echo "How many nodes do you want to create on this server?, followed by [ENTER]:"
+   read MNCOUNT
+done
+
+for i in `seq 1 1 $MNCOUNT`; do
+  echo ""
+  echo "Enter alias for new node"
+  read ALIAS
+
+  echo ""
+  echo "Enter port for node $ALIAS(i.E. 7748)"
+  read PORT
+
+  echo ""
+  echo "Enter masternode private key for node $ALIAS"
+  read PRIVKEY
+
+  echo ""
+  echo "Enter RPC Port (Any valid free port: i.E. 27261)"
+  read RPCPORT
+
+  ALIAS=${ALIAS,,}
+  CONF_DIR=~/.DingoDollars_$ALIAS
+
+  # Create scripts
+  echo '#!/bin/bash' > ~/bin/DingoDollarsd_$ALIAS.sh
+  echo "DingoDollarsd -daemon -conf=$CONF_DIR/DingoDollars.conf -datadir=$CONF_DIR "'$*' >> ~/bin/DingoDollarsd_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/DingoDollars-cli_$ALIAS.sh
+  echo "DingoDollars-cli -conf=$CONF_DIR/DingoDollars.conf -datadir=$CONF_DIR "'$*' >> ~/bin/DingoDollars-cli_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/DingoDollars-tx_$ALIAS.sh
+  echo "DingoDollars-tx -conf=$CONF_DIR/DingoDollars.conf -datadir=$CONF_DIR "'$*' >> ~/bin/DingoDollars-tx_$ALIAS.sh
+  chmod 755 ~/bin/DingoDollars*.sh
+
+  mkdir -p $CONF_DIR
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> DingoDollars.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> DingoDollars.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> DingoDollars.conf_TEMP
+  echo "rpcport=$RPCPORT" >> DingoDollars.conf_TEMP
+  echo "listen=1" >> DingoDollars.conf_TEMP
+  echo "server=1" >> DingoDollars.conf_TEMP
+  echo "daemon=1" >> DingoDollars.conf_TEMP
+  echo "logtimestamps=1" >> DingoDollars.conf_TEMP
+  echo "maxconnections=256" >> DingoDollars.conf_TEMP
+  echo "masternode=1" >> DingoDollars.conf_TEMP
+  echo "" >> DingoDollars.conf_TEMP
+
+  #echo "addnode=addnode=91.121.71.172" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=109.124.213.194" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=202.182.110.55" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=217.115.98.185" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=54.37.16.231" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=88.99.201.58" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=98.226.11.139" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=35.197.125.133" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=149.28.53.192" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=151.224.28.188" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=91.121.71.172" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=95.211.224.212" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=104.54.217.117" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=159.65.53.106" >> DingoDollars.conf_TEMP
+  #echo "addnode=addnode=173.249.33.136" >> DingoDollars.conf_TEMP
+
+  echo "" >> DingoDollars.conf_TEMP
+  echo "port=$PORT" >> DingoDollars.conf_TEMP
+  echo "masternodeaddress=$IP:$PORT" >> DingoDollars.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> DingoDollars.conf_TEMP
+  sudo ufw allow $PORT/tcp
+
+  mv DingoDollars.conf_TEMP $CONF_DIR/DingoDollars.conf
+
+  sh ~/bin/DingoDollarsd_$ALIAS.sh
+done
--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# Multi-Masternode-Setup-Script
+﻿---------
+Masternode Scripts
+---------
+
+DingoDollars - version 0.1
+
+---------
+DingoDollars Masternode Setup
+---------
+Controller-Cold-Setup
+This is the best method to setup your Masternodes. The wallet containing the coins does not have to be exposed and can run on your local computer. It does not have to run all the time.
+
+---------
+Desktop Wallet Setup
+---------
+In the first steps the desktop wallet will be setup. This is the wallet you can run on your local PC. When the masternodes are connected this wallet can be closed and the PC does not have to run in order for the masternodes to generate rewards.
+
+We will create an address, private key and transaction for each masternode (MN in the following) and show the necessary steps for configuration.
+
+1.	Open Console: Tools - Debug Console
+
+2.	Type “getaccountaddress MN1” and press Enter. Repeat for MN2, MN3, etc
+
+3.	Send exactly 2500 coins to each MN address
+
+4.	Setup MNs in config:  Tools - Open Masternode.conf
+
+Now add a Line for each MN with the following Format:
+> [Alias] [VPS IP]:[Port] [Genkey] [Txhash] [Outputindex]
+
+a.	Begin by entering and alias IP:port
+
+	i.	[Alias] the name of the MN receiving address (we named them MN1, MN2, …)
+
+	ii.	[IP] The static IP of your server
+
+	iii.	[port] A port the MN will connect to. The port is not fixed and multiple nodes can run on one server but need different ports.
+
+b.	Next we get the “masternodeprivkey “
+
+	i.	[Genkey] Open debug console again, type “masternode genkey” and press enter
+
+	ii.	Repeat the line for each MN you want to setup
+
+	iii.	Copy the output keys to the config
+
+c.	Next we will add txhash  and outputindex
+
+	i.	[Txhash] Go to the debug console again, type “masternode outputs” and press enter
+
+	ii.	[outputindex] If you send the coins in one transaction the “txhash” will be the same. If you add a masternode later, another tuple will be added. So every masternode has a unique pair of txhash and outputindex
+
+	iii.	Copy the information to the config file (Make sure you don’t forget the index!)
+
+5.	Save the file and restart the wallet.
+
+
+
+---------
+Getting a VPS
+---------
+For the cold wallets you first need a linux VPS. You can get very cheap ones for 5$ here: https://www.vultr.com/. These can run a few DINGO  masternode instances though it might be a good idea to split your MNs across some different Servers.  The more isolation you have the less likely you are hit big by a server outage?.
+
+After registration you get to the Dashboard. You have to do the first payment with something else then Bitcoin. After the first payment you can pay the servers in BTC.
+
+
+6.	Deploy the server: Hit the plus top right to add new instances
+
+7.	Choose a region near you. It does not really matter.
+
+	a.	As “Server Type” choose Ubuntu and click 14.04
+
+	b.	As Server Size choose the 5$ instance with 1GB Ram. If available you can choose the 2.50$ one if you only want to run one or two nodes on it.
+
+	c.	Scroll down and give the node a name.
+
+	d.	After that click “Deploy now” in the overview you should see this:
+
+8.	Connect to the Server.
+
+9.	Download and execute script on VPS: (each MN must have unique ports)
+
+		wget https://raw.githubusercontent.com/MikeP243/Multi-Masternode-Setup-Script/master/DINGO-setup.sh
+
+		chmod 755 DINGO-setup.sh
+
+		./DINGO-setup.sh
+
+If you make an error when typing the interactive stuff: ctrl+c and restart the script
+
+10.	Controlling the masternode
+
+a.	The script from the last step already started all wallets on VPS
+
+b.	Type the following into the VPS.
+
+		source .bashrc
+
+c.	Each MN has now its own control script under ~/bin (named with alias you typed before)
+
+d.	To see if the blocks are syncing “watch DingoDollarsd_mn1.sh getinfo”
+
+e.	If sync is finished go to your desktop wallet and start MNs in the masternode tab (unlock wallet before).
+
+f.	You can close your Desktop wallet after a while.
+
+
+---------
+Adding more nodes to existing VPS
+---------
+
+To add more DINGO MNs to a existing server setup with the setup.sh script before just restart the script and type “n” when asked if you want to install the dependencies at the beginning. After that just follow the steps from before in the interactive script.
